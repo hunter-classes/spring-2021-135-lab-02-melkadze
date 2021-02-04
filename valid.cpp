2b3bae4fc988555ae1e3d4f6719e9894dfe03653
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2021-02-04 14:23:41 -0500

    finish A

---
+++ valid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter an int such that 0 < n < 100: ";
+    
+    int input;
+    
+    std::cin >> input; //allow a valid answer before printing new text
+    
+    while (input < 1 || input > 99) {
+        std::cout << "Please re-enter: ";
+        std::cin >> input;
+    }
+    
+    std::cout << "Number squared is " << input*input << std::endl;
+    
+    return 1;
+}
