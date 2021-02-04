67942f407d0c20eaefc660a81d223256016e7f33
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2021-02-04 14:38:28 -0500

    finish B

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "funcs.h"
+
+int main() {
+    std::cout << "Result of print_interval(-5, 10): ";
+    print_interval(-5, 10);
+    
+    std::cout << "Result of print_interval(-15, 15): ";
+    print_interval(-15, 15);
+    
+    std::cout << "Result of print_interval(1, 2): ";
+    print_interval(1, 2);
+    
+    std::cout << "Result of print_interval(1000, 1007): ";
+    print_interval(1000, 1007);
+}
